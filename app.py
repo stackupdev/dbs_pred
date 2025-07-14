a9e5aaa04d8c153b978983aeea8c38f8ff2f90fb
Clarence Sai <210022556+stackupdev@users.noreply.github.com>
2025-07-14 15:59:19 +0800

    Sync

--- app.py
+++ app.py
@@ -1,35 +1,59 @@
+#
+# 
+
 from flask import Flask, render_template, request
 import joblib
+from groq import Groq
+
+import os
+os.environ['GROQ_API_KEY'] = ""
+# for cloud ..........
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    return(render_template("index.html"))
 
-@app.route("/main", methods=["GET", "POST"])
+@app.route("/main",methods=["GET","POST"])
 def main():
-    if request.method == "POST":
-        username = request.form.get("username")
-        if not username:
-            return render_template("index.html", error="Please enter your name.")
-        return render_template("main.html", username=username)
-    else:
-        return render_template("index.html")
+    q = request.form.get("q")
+    # db
+    return(render_template("main.html"))
 
-@app.route("/prediction", methods=["GET", "POST"])
+@app.route("/llama",methods=["GET","POST"])
+def llama():
+    return(render_template("llama.html"))
+
+@app.route("/llama_reply",methods=["GET","POST"])
+def llama_reply():
+    q = request.form.get("q")
+    # load model
+    client = Groq()
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": q
+            }
+        ]
+    )
+    return(render_template("llama_reply.html",r=completion.choices[0].message.content))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
+@app.route("/prediction",methods=["GET","POST"])
 def prediction():
-    if request.method == "POST":
-        try:
-            q = float(request.form.get("q"))
-            username = request.form.get("username")
-            model = joblib.load("dbs.jl")
-            pred_value = round(float(model.predict([[q]])[0]), 2)
-            return render_template("prediction.html", r=pred_value, username=username)
-        except Exception as e:
-            return f"Error: {e}", 400
-    else:
-        return render_template("index.html")
+    q = float(request.form.get("q"))
+    # load model
+    model = joblib.load("dbs.jl")
+    # make prediction
+    pred = model.predict([[q]])
+    return(render_template("prediction.html",r=pred))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
